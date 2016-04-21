1f4604f3aa4ef9b799ba8d6a17b1434fb98ac7d2
Peter Dahlbäck <p.dahlback@gmail.com>
2016-04-21 19:52:54 +0200

    Fix problem when setting WebGL build to fullscreen

--- Assets/Scripts/_Utils/CameraAspectAdjuster.cs
+++ Assets/Scripts/_Utils/CameraAspectAdjuster.cs
@@ -6,33 +6,57 @@ public class CameraAspectAdjuster : MonoBehaviour
 	public Camera mainCamera;
 	public float targetAspectRatio;
 
-	// Use this for initialization
+	private float latestWindowAspectRatio;
+
 	void Awake ()
 	{
+		latestWindowAspectRatio = targetAspectRatio;
+		AdjustIfNeeded ();
+	}
+
+	void Update ()
+	{
+		AdjustIfNeeded ();
+	}
+
+	void AdjustIfNeeded ()
+	{
 		float windowAspectRatio = (float)Screen.width / (float)Screen.height;
-		float scale = windowAspectRatio / targetAspectRatio;
+		if (windowAspectRatio != latestWindowAspectRatio) {
+			float scale = windowAspectRatio / targetAspectRatio;
 
-		// if scaled height is less than current height, add letterbox
-		if (scale < 1.0f) {  
-			Rect rect = mainCamera.rect;
+			// if scaled height is less than current height, add letterbox
+			if (scale < 1.0f) {  
+				Rect rect = mainCamera.rect;
 
-			rect.width = 1.0f;
-			rect.height = scale;
-			rect.x = 0;
-			rect.y = (1.0f - scale) / 2.0f;
+				rect.width = 1.0f;
+				rect.height = scale;
+				rect.x = 0;
+				rect.y = (1.0f - scale) / 2.0f;
 
-			mainCamera.rect = rect;
-		} else if (scale > 1.0f) { // add pillarbox
-			float scalewidth = 1.0f / scale;
+				mainCamera.rect = rect;
+			} else if (scale > 1.0f) { // add pillarbox
+				float scalewidth = 1.0f / scale;
 
-			Rect rect = mainCamera.rect;
+				Rect rect = mainCamera.rect;
 
-			rect.width = scalewidth;
-			rect.height = 1.0f;
-			rect.x = (1.0f - scalewidth) / 2.0f;
-			rect.y = 0;
+				rect.width = scalewidth;
+				rect.height = 1.0f;
+				rect.x = (1.0f - scalewidth) / 2.0f;
+				rect.y = 0;
 
-			mainCamera.rect = rect;
+				mainCamera.rect = rect;
+			} else {
+				Rect rect = mainCamera.rect;
+
+				rect.width = 1.0f;
+				rect.height = 1.0f;
+				rect.x = 0;
+				rect.y = 0;
+
+				mainCamera.rect = rect;
+			}
+			latestWindowAspectRatio = windowAspectRatio;
 		}
 	}
 }
